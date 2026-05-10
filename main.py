85361c64a5fd28b9e60786c05c5110ce747ba0c4
Martins Kalnins <kalninsmartins@gmail.com>
2026-05-10 17:20:19 +0300

    2_solis+polimorfisms

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ temp = WeatherData("Riga", [1,2,3], [4,5,7])
 average = temp.calculate_average()
 
 print(average)    
+#testē - polimorfisms, jo 'show_data_type()' uzvedas dažādi dažādos objektos. Ktra mantojošā klase uz šo modeli reaģē atšķirīgi
+from weather_models import *
+
+temp = TemperatureData("Riga", [1,2], [4,5])
+humid = HumidityData("Riga", [1,2], [80,82])
+wind = WindSpeedData("Riga", [1,2], [12,15])
+
+temp.show_data_type()
+humid.show_data_type()
+wind.show_data_type()

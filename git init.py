0b349f7f3e33886d81edc39482a0a1f0f34957c1
Martins Kalnins <kalninsmartins@gmail.com>
2026-05-10 10:58:41 +0300

    1_solis_struktura+klase

---
+++ git init.py
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit - environment setup"
+git remote add origin https://github.com/KalMar1984/Programmas_in-enierija_un_lietot-u_uztur-ana.git
+git branch -M main
+git push -u origin main
